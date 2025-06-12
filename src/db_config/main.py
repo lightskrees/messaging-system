e9443e0e20d393eabe494d9660d3ac21a7449faa
lightskrees <nchris3010@gmail.com>
2025-06-12 10:47:25 +0200

    added session dependency configuration

---
+++ src/db_config/main.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from typing import Annotated
+from fastapi import Depends
+
+from src.config import Config as settings
+
+async_engine = AsyncEngine(create_engine(settings.DATABASE_URL, echo=True))
+
+async def create_db_tables():
+    async with async_engine.connect() as conn:
+        await conn.run_sync(SQLModel.metadate.create_all)
+
+async def get_session():
+    session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with session() as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
